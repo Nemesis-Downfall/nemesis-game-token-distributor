4ec63cabfe56793947f2f10ab78353ad7265f955
Kingsley Victor <javaprodigy56@gmail.com>
2023-01-01 16:43:13 +0100

    init

--- contracts/libraries/ECDSALib.sol
+++ contracts/libraries/ECDSALib.sol
@@ -5,12 +5,17 @@ library ECDSALib {
     prefixedHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
   }
 
-  function split(bytes memory signature) internal pure returns (bytes32 r, bytes32 s, uint256 v) {
-    require(signature.length == 65, 'invalid_signature_length');
+  function split(bytes memory signature) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
+    require(signature.length == 65, "invalid_signature_length");
     assembly {
       r := mload(add(signature, 32))
       s := mload(add(signature, 64))
       v := byte(0, mload(add(signature, 96)))
     }
+  }
+
+  function obtainSigner(bytes32 messageHash, bytes memory signature) internal pure returns (address signer) {
+    (bytes32 r, bytes32 s, uint8 v) = split(signature);
+    signer = ecrecover(messageHash, v, r, s);
   }
 }
