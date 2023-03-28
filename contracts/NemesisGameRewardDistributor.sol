0a87d1bff9dde64a8598080ba94403a339d13b62
Kingsley Victor <javaprodigy56@gmail.com>
2023-03-28 16:38:59 +0100

    made updates

--- contracts/NemesisGameRewardDistributor.sol
+++ contracts/NemesisGameRewardDistributor.sol
@@ -1,8 +1,34 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./libraries/ECDSALib.sol";
+import "./libraries/TransferHelpers.sol";
 
-contract NemesisGameRewardDistributor is Ownable {
+contract NemesisGameRewardDistributor is Ownable, AccessControl {
   using ECDSALib for bytes32;
+  using Address for address;
+
+  bytes32 constant DISTRIBUTOR_ROLE = keccak256(abi.encodePacked("DISTRIBUTOR_ROLE"));
+  address token;
+
+  constructor(address distributor, address _token) {
+    require(_token.isContract(), "must_be_contract_address");
+    token = _token;
+    _grantRole(DISTRIBUTOR_ROLE, distributor);
+  }
+
+  function canClaim(address claimant, bytes32 messageHash, bytes memory signature) private pure returns (bool) {
+    bytes32 prefixedHashMessage = messageHash.prefixed();
+    return claimant == prefixedHashMessage.obtainSigner(signature);
+  }
+
+  function distributeReward(address claimant, bytes32 messageHash, bytes memory signature, uint256 reward) external {
+    require(hasRole(DISTRIBUTOR_ROLE, _msgSender()), "only_distributor");
+    require(canClaim(claimant, messageHash, signature), "player_can't_claim_now");
+    require(IERC20(token).balanceOf(address(this)) >= reward, "not_enough_balance");
+    TransferHelpers._safeTransferERC20(token, claimant, reward);
+  }
 }
--- contracts/libraries/TransferHelpers.sol
+++ contracts/libraries/TransferHelpers.sol
@@ -10,22 +10,13 @@ library TransferHelpers {
     require(success, "failed to transfer ether");
   }
 
-  function _safeTransferERC20(
-    address token,
-    address to,
-    uint256 amount
-  ) internal returns (bool success) {
+  function _safeTransferERC20(address token, address to, uint256 amount) internal returns (bool success) {
     require(token.isContract(), "call_to_non_contract");
     (success, ) = token.call(abi.encodeWithSelector(bytes4(keccak256(bytes("transfer(address,uint256)"))), to, amount));
     require(success, "low_level_contract_call_failed");
   }
 
-  function _safeTransferFromERC20(
-    address token,
-    address spender,
-    address recipient,
-    uint256 amount
-  ) internal returns (bool success) {
+  function _safeTransferFromERC20(address token, address spender, address recipient, uint256 amount) internal returns (bool success) {
     require(token.isContract(), "call_to_non_contract");
     (success, ) = token.call(abi.encodeWithSelector(bytes4(keccak256(bytes("transferFrom(address,address,uint256)"))), spender, recipient, amount));
     require(success, "low_level_contract_call_failed");
